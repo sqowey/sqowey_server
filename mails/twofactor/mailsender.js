2f57fa52e52a3e05427a67f5cd70450f1fea22bc
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-05-06 15:39:25 +0200

    added logging

--- mails/twofactor/mailsender.js
+++ mails/twofactor/mailsender.js
@@ -47,46 +47,59 @@ var mail_transporter = nodemailer.createTransport({
 
 // Update the mail in the database
 connection.query(`UPDATE ` + config.db_tables.twofactor_mails.table + ` SET sent = "` + current_timestamp + `" WHERE sent IS NULL`, function(err, rows, fields) {
-    console.log("Database updated: " + rows.affectedRows + " rows affected");
-    console.log("Sending mails...");
+    if (err) throw err;
 });
 
 // Get all mails from the database that are not sent yet
 connection.query(`SELECT * FROM ` + config.db_tables.twofactor_mails.table + ` WHERE sent = "` + current_timestamp + `"`, function(err, rows, fields) {
 
-    // Loop through the rows
-    for (var i = 0; i < rows.length; i++) {
+    // Check if there are any mails to send
+    if (rows.length > 0) {
 
-        // Get the values
-        reciever = rows[i].reciever;
-        username = rows[i].username;
-        code = rows[i].code;
+        // Log
+        console.log("Sending 2FA-mails...");
 
-        // Get the mail template
-        mail_template = fs.readFileSync("./mails/twofactor/template.html", 'utf8');
+        // Loop through the rows
+        for (var i = 0; i < rows.length; i++) {
 
-        // Replace the placeholders in the mail template
-        mail_body = mail_template.replace("?USERNAME?", username).replace("?VERIFICATIONCODE?", code);
+            // Get the values
+            reciever = rows[i].reciever;
+            username = rows[i].username;
+            code = rows[i].code;
 
-        // Send the mail
-        mail_transporter.sendMail({
-            from: config.mail.auth.user,
-            to: reciever,
-            subject: "Sqowey 2FA",
-            html: mail_body
-        }, function(error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
+            // Get the mail template
+            mail_template = fs.readFileSync("./mails/twofactor/template.html", 'utf8');
+
+            // Replace the placeholders in the mail template
+            mail_body = mail_template.replace("?USERNAME?", username).replace("?VERIFICATIONCODE?", code);
+
+            // Send the mail
+            mail_transporter.sendMail({
+                from: config.mail.auth.user,
+                to: reciever,
+                subject: "Sqowey 2FA",
+                html: mail_body
+            }, function(error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+
+            // Check if last iteration
+            if (i == rows.length - 1) {
+
+                // End connection to the database and close the transporter
+                connection.end();
             }
-        });
-
-        // Check if last iteration
-        if (i == rows.length - 1) {
-
-            // End connection to the database and close the transporter
-            connection.end();
         }
+    } else {
+
+        // Log
+        console.log("No 2FA-mails to send");
+
+        // Close
+        connection.end();
     }
 });
