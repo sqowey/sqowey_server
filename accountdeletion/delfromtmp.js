ced36d655e54cda7202cd34b149da2d822ec9ad8
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-05-17 14:57:19 +0200

    added tmp_deleter

---
+++ accountdeletion/delfromtmp.js
@@ -0,0 +1,45 @@
+// !WARNING!
+// THIS FILE HAS TO BE RUN FROM THE ROOT DIRECTORY (FROM THE PARENT FOLDER OF THE FOLDER WHERE THIS FILE IS)
+// !WARNING!
+
+
+// 
+// Modules
+// 
+const mysql = require('mysql');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+
+// 
+// Variables
+// 
+const config = require('../config.json');
+const { time } = require('console');
+var username;
+var timestamp;
+var email;
+var sql;
+
+// 
+// Main
+//
+
+// Create a connection to the database
+var tmp_connection = mysql.createConnection({
+    host: config.db_tables.tmp_accountdeletion.host,
+    user: config.db_tables.tmp_accountdeletion.user,
+    password: config.db_tables.tmp_accountdeletion.pass,
+    database: config.db_tables.tmp_accountdeletion.db
+});
+tmp_connection.connect();
+
+
+// Get the id and timestamp from the database
+tmp_connection.query(`DELETE FROM ` + config.db_tables.tmp_accountdeletion.table + " WHERE mails_created = 1;", function(err, result) {
+
+    // If error
+    if (err) throw err;
+
+    // Close the connection
+    tmp_connection.end();
+});
