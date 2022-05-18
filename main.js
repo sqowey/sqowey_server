53ca882c6dcdb2767dfde894b5ba0a7f5a231c30
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-05-18 21:34:33 +0200

    added account deletion functions

--- main.js
+++ main.js
@@ -25,3 +25,38 @@ setInterval(function() {
     });
 
 }, 10000);
+
+// Loop every 1 minute
+setInterval(function() {
+
+    // Run ./mails/accdeletion/mailsender.js
+    console.log("Account-deletion-Mailer running");
+    exec('node ./mails/accdeletion/mailsender.js', function(error, stdout, stderr) {
+        if (error) {
+            console.log(error);
+        }
+        if (stderr) {
+            console.log(stderr);
+        }
+    });
+
+    // Run ./accdeletion/delfromtmp.js
+    // Run ./accdeletion/tmptomail.js
+    console.log("Account-deletion-cleaneup running");
+    exec('node ./accountdeletion/delfromtmp.js', function(error, stdout, stderr) {
+        if (error) {
+            console.log(error);
+        }
+        if (stderr) {
+            console.log(stderr);
+        }
+    });
+    exec('node ./accountdeletion/tmptomail.js', function(error, stdout, stderr) {
+        if (error) {
+            console.log(error);
+        }
+        if (stderr) {
+            console.log(stderr);
+        }
+    });
+}, 60000);
