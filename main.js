3b93a5a6478ea3f627cf6da8515f28540fad690a
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-05-18 21:17:17 +0200

    fixed 10 second time

--- main.js
+++ main.js
@@ -19,20 +19,10 @@ setInterval(function() {
     // Decrease the timer
     timer--;
 
-    // Check if 10 seconds have passed
-    if (timer <= 0) {
-        timer = 10;
+    // Run the function to run the other script files
+    run_10s_scripts();
 
-        console.log();
-
-        // Run the function to run the other script files
-        run_10s_scripts();
-
-    } else {
-        console.log("Waiting [" + timer + "/10]");
-    }
-
-}, 1000);
+}, 10000);
 
 // Function that runs all stuff that needs to be done every 10 seconds
 function run_10s_scripts() {
