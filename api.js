dff3f326838d6b1479f2bc3323f97a3342697588
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-08-22 13:55:06 +0200

    add main structure

---
+++ api.js
@@ -0,0 +1,13 @@
+// Get the express module
+const express = require("express");
+
+// Create the express Instance
+const API = express();
+
+// Set up the api to get json requests
+API.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+API.use(express.json());
