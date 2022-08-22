67dce17ebc2c6895f534e23c7a891fd2274b0334
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-08-22 14:30:43 +0200

    add auth request body checker

--- api.js
+++ api.js
@@ -15,6 +15,18 @@ API.use(
 );
 API.use(express.json());
 
+// Auth endpoint
+API.get("/auth/", (req, res) => {
+    // Get the body
+    const body = req.body;
+    const parsedbody = body; //JSON.parse(body);
+    // Check if body is right
+    if (!parsedbody.app_id || !parsedbody.app_secret) {
+        res.status(400);
+        res.json({ "error": "Bad request", "hint": "Your request is missing something! Please look into the Documentation" });
+        return;
+    }
+});
 
 // Run the express server
 API.listen(port, () => {
