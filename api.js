44ee24470d6fcdedf612a8204aada211472d8701
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-08-23 13:03:19 +0200

    Remove parsedbody variable

--- api.js
+++ api.js
@@ -27,45 +27,44 @@ API.use(express.json());
 // Auth endpoint
 API.get("/auth/", (req, res) => {
     // Get the body
-    const body = req.body;
-    const parsedbody = body; //JSON.parse(body);
+    const requestbody = req.body;
     // Check if body is right
-    if (!parsedbody.app_id || !parsedbody.app_secret) {
+    if (!requestbody.app_id || !requestbody.app_secret) {
         res.status(400);
         res.json({ "error": "Bad request", "hint": "Your request is missing something! Please look into the Documentation" });
-        api_log.writeLog("GET", "/AUTH/", 400, { "app_id": parsedbody.app_id });
+        api_log.writeLog("GET", "/AUTH/", 400, { "app_id": requestbody.app_id });
         return;
     }
     // Check if the app id matches the app secret
-    devportal_db_connection.query("SELECT app_secret FROM apps WHERE app_id = \"" + parsedbody.app_id + "\"", function(error, results, fields) {
+    devportal_db_connection.query("SELECT app_secret FROM apps WHERE app_id = \"" + requestbody.app_id + "\"", function(error, results, fields) {
         if (error) throw error;
         // Check if app can be found by id
         if (results == false) {
             res.status(401);
             res.json({ "error": "Unknown app ID", "hint": "The used app id is unknown! Please check the devportal!" });
-            api_log.writeLog("GET", "/AUTH/", 401, { "app_id": parsedbody.app_id });
+            api_log.writeLog("GET", "/AUTH/", 401, { "app_id": requestbody.app_id });
             return;
         }
         // Check if id matches secret
-        if (results[0].app_secret != parsedbody.app_secret) {
+        if (results[0].app_secret != requestbody.app_secret) {
             res.status(403);
             res.json({ "error": "Wrong App secret", "hint": "The used app secret is wrong! Please check the devportal!" });
-            api_log.writeLog("GET", "/AUTH/", 403, { "app_id": parsedbody.app_id });
+            api_log.writeLog("GET", "/AUTH/", 403, { "app_id": requestbody.app_id });
             return;
         }
         // Delete old auth token entry
-        devportal_db_connection.query("DELETE FROM authentification WHERE app_id = '" + parsedbody.app_id + "'");
+        devportal_db_connection.query("DELETE FROM authentification WHERE app_id = '" + requestbody.app_id + "'");
         // Create new auth token
         var auth_token = "";
         for (let i = 1; i < 48; i++) {
             auth_token += config.endpointSettings.auth.tokenChars.charAt(Math.floor(Math.random() * config.endpointSettings.auth.tokenChars.length));
         }
         // Insert the auth token
-        devportal_db_connection.query("INSERT INTO authentification (app_id, auth_token) VALUES ('" + parsedbody.app_id + "', '" + auth_token + "')");
+        devportal_db_connection.query("INSERT INTO authentification (app_id, auth_token) VALUES ('" + requestbody.app_id + "', '" + auth_token + "')");
         // Respond with auth token
         res.status(201);
-        res.json({ "app_id": parsedbody.app_id, "auth_token": auth_token });
-        api_log.writeLog("GET", "/AUTH/", 201, { "app_id": parsedbody.app_id, "auth_token": auth_token });
+        res.json({ "app_id": requestbody.app_id, "auth_token": auth_token });
+        api_log.writeLog("GET", "/AUTH/", 201, { "app_id": requestbody.app_id, "auth_token": auth_token });
     });
 });
 
