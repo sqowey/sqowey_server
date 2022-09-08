3a9ac0fde2b4f12dfe62cf72d79c762cf10b50ae
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-09-09 00:11:06 +0200

    Add token reset connection

--- api/tokens/reset.js
+++ api/tokens/reset.js
@@ -3,3 +3,14 @@ const config = require("../../config.json");
 // Get the mysql module
 const mysql = require("mysql");
 
+
+// Create a connection variable
+var conn = mysql.createConnection(config.general.mysql_connections.application);
+// Connect to the database
+conn.connect(function(err) {
+    if (err) {
+        console.error(config.general.log_messages.mysql.error.token_reset + err.stack);
+        return;
+    }
+    console.log(config.general.log_messages.mysql.connect.token_reset + conn.threadId);
+});
