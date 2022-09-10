4c8118cb5cb4cc35c71b1eb6a20c00c72b4eed6b
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-09-10 17:21:59 +0200

    Add changes to logging

--- api/api.js
+++ api/api.js
@@ -330,7 +330,7 @@ API.patch("/applications/", (req, res) => {
     if (!requestbody.app_name || !requestbody.dev_id || !requestbody.changes) {
         res.status(400);
         res.json(config.api.messages.error.badRequest);
-        api_log.writeLog("PATCH", "/APPLICATIONS/", 400, { "app_name": requestbody.app_name, "dev_id": requestbody.dev_id });
+        api_log.writeLog("PATCH", "/APPLICATIONS/", 400, { "app_name": requestbody.app_name, "dev_id": requestbody.dev_id, "changes": requestbody.changes });
         return;
     }
 });
